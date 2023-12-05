064bd2fdc79fd407d6ae52ffe624f83baeaffe84
cbkinase <cambeck366@gmail.com>
2023-12-05 04:20:41 -0500

    Newton's method for sqrt and cube roots

---
+++ sqrt_and_exercise_1.8.scm
@@ -0,0 +1,43 @@
+(define TOLERANCE 0.0001)
+
+(define (square x) (* x x))
+
+(define (abs x)
+    (cond
+        ((< x 0) (- x))
+        (else x)))
+
+(define (average-iter good-enough? improve guess x)
+(if (good-enough? guess x)
+    guess
+    (average-iter good-enough? improve (improve guess x) x)))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (sqrt-improve guess x)
+    (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) TOLERANCE))
+
+(define (sqrt x)
+    (average-iter good-enough? sqrt-improve 1.0 x))
+
+(display (sqrt 9))
+(newline)
+
+;;; Newton's method for cube roots
+;;; Exercise 1.8
+
+(define (cube-root-improve guess x)
+  (/ (+ (/ x (square guess)) (* guess 2)) 3))
+
+(define (cube-root-good-enough? guess x)
+    (< (abs (- (* guess guess guess) x)) TOLERANCE))
+
+(define (cube-root x)
+    (average-iter cube-root-good-enough? cube-root-improve 1.0 x))
+
+(display (cube-root 8))
+(newline)
